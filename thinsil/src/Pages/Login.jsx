c0a5503d12cd4f2db04536bfa22e1d0882f69a2e
ankush3681 <ankushkumar83623@gmail.com>
2023-10-20 16:46:47 +0530

    login and signup responsive

--- thinsil/src/Pages/Login.jsx
+++ thinsil/src/Pages/Login.jsx
@@ -1,20 +1,26 @@
 import React from 'react';
-import "../App.css";
+import "../styles/login.css";
+import { FcUnlock } from "react-icons/fc";
+import { Link } from "react-router-dom";
 
 const Login = () => {
   return (
     <div className='login-container'>
-      <h1>Login Here</h1>
+      <FcUnlock className='login-icon'/>
+      <h1>Login</h1>     
       <form className='login-form'>
         <label>Email<span>*</span> 
         <input type="text" /></label>
         <label>Password<span>*</span> 
-        <input type="text" /></label>
+        <input type="password" /></label>
         <div > 
-        <input type="checkbox" style={{border:"1px solid red"}}/> Remember me</div>
+        <input type="checkbox"/> Remember me</div>
         <input type='submit' value={"Login"}/>
       </form>
-      <div></div>
+      <div className='login-links'>
+         <div>forgot password?</div>
+          <div>Don't have account?<Link className="signup-url-in-login" to="/signup"> SignUp</Link></div>
+      </div>
     </div>
   )
 }
--- thinsil/src/Pages/SignUp.jsx
+++ thinsil/src/Pages/SignUp.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+
+import React from 'react';
+import "../styles/signup.css";
+import { FcLock } from "react-icons/fc";
+import { Link } from "react-router-dom";
 
 const SignUp = () => {
   return (
-    <div>
-      
+    <div className='signup-container'>
+      <FcLock className='signup-icon'/>
+      <h1>Sign up</h1>     
+      <form className='signup-form'>
+        <label>Name<span>*</span> 
+        <input type="text" /></label>
+        <label>Email<span>*</span> 
+        <input type="email" /></label>
+        <label>Password<span>*</span> 
+        <input type="password" /></label>
+        <div className='signup-checkbox'> 
+        <input type="checkbox"/>By continuing, you agree to Thensil Terms and Policy.</div>
+        <input type='submit' value={"Sign up"}/>
+      </form>
+          <Link className='signup-Link-in-login' to="/login">Already have user? <span>Login</span></Link>
     </div>
   )
 }
 
-export default SignUp
+export default SignUp;
