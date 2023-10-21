d1af1eb65bce02e937cd6268424988b72c3eaf19
ankush3681 <ankushkumar83623@gmail.com>
2023-10-21 13:36:38 +0530

    responsive navbar

--- thinsil/src/App.js
+++ thinsil/src/App.js
@@ -1,19 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
 import { Routes,Route } from 'react-router-dom';
 import Product from './Pages/Product';
 import Login from './Pages/Login';
 import SignUp from './Pages/SignUp';
 import Cart from './Pages/Cart';
+import Navbar from './component/Navbar';
 
 function App() {
   return (
+    <div>
+      <Navbar/>
     <Routes>
       <Route path="/" element={< Product/>} />
       <Route path="/login" element={< Login/>} />
       <Route path="/signup" element={< SignUp/>} />
       <Route path="/cart" element={< Cart/>} />
     </Routes>
+    </div>
   );
 }
 
--- thinsil/src/component/Navbar.jsx
+++ thinsil/src/component/Navbar.jsx
@@ -1,11 +1,63 @@
-import React from 'react'
+import React, { useState } from "react";
+import logo from "./logo.png";
+import { Link } from "react-router-dom";
+import { GiHamburgerMenu } from "react-icons/gi";
+import "../styles/Navbar.css";
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMidNav = () => {
+    setIsOpen(!isOpen);
+    console.log(isOpen);
+    
+  };
+  window.addEventListener("resize",()=>{
+    if (window.innerWidth > 800){
+      setIsOpen(false);
+    }
+  })
   return (
     <div>
-      
-    </div>
-  )
-}
+      <nav className="navbar">
+        <div className="nav-logo">
+          <Link to="/" ><img src={logo} alt="logo" /></Link>
+          
+        </div>
+        <div className="mid-nav-section">
+          <Link to="/" className="nav-link">
+            Home
+          </Link>
+          <Link to="/cart" className="nav-link">
+            Cart
+          </Link>
+        </div>
 
-export default Navbar
+        <div className="end-nav-section">
+          <Link to="/login" className="nav-login">
+            Login
+          </Link>
+          <div className="burger-menu" onClick={toggleMidNav}>
+            <GiHamburgerMenu className="burger-icon" />
+          </div>
+        </div>
+      </nav>
+      {isOpen && (
+        <div className="dropdown-nav-menu">
+          <Link to="/" className="nav-link">
+            Home
+          </Link>
+          <Link to="/cart" className="nav-link">
+            Cart
+          </Link>
+          <Link to="/login" className="nav-link">
+            Login
+          </Link>
+        </div>
+      )}
+    </div>
+
+  );
+};
+
+export default Navbar;
